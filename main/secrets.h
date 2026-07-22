11c006e978fd408254c3ae396192aeb88dc06966
Wilson Cosmo <97473599+WCosmo@users.noreply.github.com>
2026-07-22 14:20:41 -0300

    Delete main directory

--- main/secrets.h
+++
@@ -1,8 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "A"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "B"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID C			// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "D"   // replace XYZ with your channel write API Key
