060faf8018c8527c0f90ac25a3333a2b097b36fe
Wilson Cosmo <97473599+WCosmo@users.noreply.github.com>
2026-07-30 18:35:42 -0300

    Delete monitoramento_IoT directory

--- monitoramento_IoT/secrets.h
+++
@@ -1,9 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "LCT"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "propagacao2025"	// replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID_WEATHER_STATION 3430238	          	//MathWorks weather station
-
-#define SECRET_READ_APIKEY_COUNTER "LY5VSWBLO2RAZG50"	//API Key for Test channel
